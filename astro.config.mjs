2e2372ff8baec1644579d01c2460fb2f7b264c4e
sam0528 <sam0528@tt186.com>
2023-05-16 00:43:32 +0800

    刪除blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-    // site: 'https://example.com',
-    // integrations: [mdx(), sitemap()],
-    // output: "server",
-    // adapter: netlify(),
+    site: "https://samwangblog.netlify.app",
+    integrations: [mdx(), sitemap()],
 });
