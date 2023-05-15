e2c6d867763344c647bdd15f125f403737228c37
sam0528 <sam0528@tt186.com>
2023-05-15 23:22:42 +0800

    首次提交

---
+++ src/content/config.ts
@@ -0,0 +1,13 @@
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+    // Type-check frontmatter using a schema
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        // Transform string to Date object
+        heroImage: z.string().optional(),
+    }),
+});
+
+export const collections = { blog };
